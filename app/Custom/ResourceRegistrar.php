5e771f33c34a930489163c05daf365961c811dd5
Eduardo Ferreira <edu.nascimento22@outlook.com>
2020-01-30 17:25:39 -0300

    Created export to excel resource

---
+++ app/Custom/ResourceRegistrar.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Custom;
+
+use Illuminate\Routing\ResourceRegistrar as OriginalRegistrar;
+
+class ResourceRegistrar extends OriginalRegistrar
+{
+    // add data to the array
+    /**
+     * The default actions for a resourceful controller.
+     *
+     * @var array
+     */
+    protected $resourceDefaults = ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy', 'exportToExcel'];
+
+
+    /**
+     * Add the data method for a resourceful route.
+     *
+     * @param  string  $name
+     * @param  string  $base
+     * @param  string  $controller
+     * @param  array   $options
+     * @return \Illuminate\Routing\Route
+     */
+    protected function addResourceExportToExcel($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name).'/exportToExcel';
+
+        $action = $this->getResourceAction($name, $controller, 'exportToExcel', $options);
+
+        return $this->router->post($uri, $action);
+    }
+}
